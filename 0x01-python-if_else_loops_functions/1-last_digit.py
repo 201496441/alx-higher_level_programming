8a1245b4dc972e845b755679175947f7054a7a4c
201496441 <magorothabo1@gmail.com>
2023-03-07 16:41:45 +0000

    fix bug

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
+
 if number >= 0:
     last_digit = number % 10
 else:
@@ -14,4 +15,3 @@ elif last_digit > 5:
     print(message, "greater than 5")
 else:
     print(message, "less than 6 and not 0")
-

2acfc2f43bd8933f23c811f7d7df25f684ce6862
201496441 <magorothabo1@gmail.com>
2023-03-09 13:45:13 +0200

    pycodestyle fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    """Print Args"""
-    import sys
-count = len(sys.argv) - 1
+import sys
 
-if count == 0:
-    print("0 arguments")
-elif count == 1:
-    print("1 arguments")
-else:
-    print("{} arguments".format(count))
-for i in range(count):
-    print("{} {}".format(i + 1, sys.argv[i + 1]))
+if __name__ == '__main__':
+    args = sys.argv[1:]  # get all arguments except the program name itself
+    n_args = len(args)  # count the number of arguments
+
+    # print the number of arguments and the list of arguments
+    if n_args == 0:
+        print("0 arguments.")
+    elif n_args == 1:
+        print("1 argument:", args[0])
+    else:
+        print(n_args, "arguments:", end=" ")
+        for i, arg in enumerate(args):
+            print(f"\n{i + 1}: {arg}")
+

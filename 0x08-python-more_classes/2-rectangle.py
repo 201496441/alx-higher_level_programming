9c6da19399a59833ed7b13305e463efb36b43480
201496441 <magorothabo1@gmail.com>
2023-04-03 13:25:53 +0200

    task 3

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -23,14 +23,14 @@ class Ractange:
     @height.setter
     def height(self, value):
         if not isinstance(Value , int):
-            raise TypeError("width must be an integer")
+            raise TypeError("height must be an integer")
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("height must be >= 0")
         self.__height = value
     def area(self):
         return (self.__width*self.__height)
     def perimeter(self):
-        if self.__width = 0 or self.__height = 0:
+        if self.__width == 0 or self.__height == 0:
             return (0)
         else:
             return 2*(self.__width + self.__height)

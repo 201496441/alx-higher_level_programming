0649345e6de420024e93cdcf7dcf2847ce1c0f4f
201496441 <magorothabo1@gmail.com>
2023-04-12 12:57:25 +0200

    fix errors

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 def read_file(filename=""):
 
-    with open(filename, encoding ="utf-8") as f:
+    with open(filename, encoding = "utf-8") as f:
         read_data = f.read()
         print(read_data, end = "")

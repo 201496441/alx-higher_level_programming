93d79ac168378f49c0b8a0957e735fd6b69c3b68
201496441 <magorothabo1@gmail.com>
2023-07-25 12:14:41 -0700

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,44 @@
-#!/usr/bin/node
-// A script that computes the number of tasks completed by user id.
+#!/usr/bin/env node
 
-const args = process.argv;
-let reqURL = args[2];
-let request = require('request');
-request(reqURL, function (error, response, body) {
-  if (error) {
-    console.log('error:', error);
-  } else {
-    let todos = JSON.parse(body);
-    let dash = {};
-    for (let i = 0; i < todos.length; i++) {
-      let status = (todos[i]['completed']);
-      let key = todos[i]['userId'].toString();
-      if (status) {
-        if (dash[key]) {
-          dash[key]++;
+const request = require('request');
+
+// Function to compute the number of completed tasks by user ID
+function countCompletedTasks(apiUrl) {
+  request(apiUrl, (error, response, body) => {
+    if (error) {
+      console.error('Error:', error);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      console.error('API request failed with status:', response.statusCode);
+      return;
+    }
+
+    const todos = JSON.parse(body);
+    const completedTasks = {};
+
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        if (completedTasks[todo.userId]) {
+          completedTasks[todo.userId]++;
         } else {
-          dash[key] = 1;
+          completedTasks[todo.userId] = 1;
         }
       }
-    }
-    console.log(dash);
-  }
-});
+    });
+
+    console.log(completedTasks);
+  });
+}
+
+// Get the API URL from the command-line arguments
+const apiUrl = process.argv[2];
+
+// Check if the API URL is provided
+if (!apiUrl) {
+  console.error('Usage: ./6-completed_tasks.js <API_URL>');
+  process.exit(1);
+}
+// Call the function to compute the number of completed tasks
+countCompletedTasks(apiUrl);
